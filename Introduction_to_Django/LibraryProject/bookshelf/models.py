0d78edaeecd394125d7868298001ed2a9132979d
Collins <102311172+Collins-Okyere@users.noreply.github.com>
2025-02-17 22:53:37 +0000

    Delete Introduction_to_Django/LibraryProject directory

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Book(models.Model):
-    title = models.CharField(max_length=200, blank=False)  # Required field
-    author = models.CharField(max_length=100, blank=False)  # Required field
-    publication_year = models.IntegerField(blank=False)  # Required field
-
-    def __str__(self):
-        return self.title
