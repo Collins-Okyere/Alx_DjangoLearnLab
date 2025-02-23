a7c5a9dd1d75d0605f499d7c396d817ea859df29
CwequKeys <cagyeiofficial@gmail.com>
2025-02-23 15:37:51 +0000

    auth done

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from .models import Book,Library
 
 def list_books(request):
