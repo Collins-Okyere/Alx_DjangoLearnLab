71e3ed14b6b866086adf9d865e6cbc45b8fba6e0
CwequKeys <cagyeiofficial@gmail.com>
2025-02-23 15:36:17 +0000

    librariam corrected

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,8 @@
 from django.views.generic.detail import DetailView
-from django.shortcuts import render
-from django.views.generic import DetailView
-from .models import Book
-from .models import Library
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from .models import Book,Library
 
 def list_books(request):
     books = Book.objects.all()
@@ -12,3 +12,33 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+
+# User login view
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('list_books')  # Redirect to books page after login
+    else:
+        form = AuthenticationForm()
+    return render(request, 'relationship_app/login.html', {'form': form})
+
+# User logout view
+def user_logout(request):
+    logout(request)
+    return render(request, 'relationship_app/logout.html')
+
+# User registration view
+def user_register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log in the user after registration
+            return redirect('list_books')  # Redirect to books page after registration
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
