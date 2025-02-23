f04bc7752061718cef1f51501eb6ef173aab4849
CwequKeys <cagyeiofficial@gmail.com>
2025-02-23 15:50:21 +0000

    auth done

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import LibraryDetailView
 from django.contrib.auth.views import LoginView, LogoutView
-from . import views
+from . import views, admin_view, librarian_view, member_view
 
 urlpatterns = [
     # Authentication URLs
@@ -12,4 +12,7 @@ urlpatterns = [
     # Other views
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'), 
+    path('admin-view/', admin_view.admin_view, name='admin_view'),
+    path('librarian-view/', librarian_view.librarian_view, name='librarian_view'),
+    path('member-view/', member_view.member_view, name='member_view'),
 ]
