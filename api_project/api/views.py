8b5016be5f6fb7de6990e1729d201d4d310436f1
CwequKeys <cagyeiofficial@gmail.com>
2025-03-10 18:09:47 +0000

    RESTful api init

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,10 +1,10 @@
-from rest_framework.generics import ListAPIView
+from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
-class BookList(ListAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all() 
     serializer_class = BookSerializer 
 
