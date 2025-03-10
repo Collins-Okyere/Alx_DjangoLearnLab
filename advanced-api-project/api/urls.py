516466cbcb26114e5040d4290bef8e64c0451bf2
CwequKeys <cagyeiofficial@gmail.com>
2025-03-10 20:45:00 +0000

    generic views corrected

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),  # Retrieve a book
     path('books/create/', BookCreateView.as_view(), name='book-create'),  # Create a new book
     path('books/update/<int:pk>', BookUpdateView.as_view(), name='book-update'),  # Update a book
-    path('books//delete/<int:pk>', BookDeleteView.as_view(), name='book-delete'),  # Delete a book
+    path('books/delete/<int:pk>', BookDeleteView.as_view(), name='book-delete'),  # Delete a book
 ]
