5ba5ddcf9b307fca0f4f2e29523f4d7f7be98d03
CwequKeys <cagyeiofficial@gmail.com>
2025-03-10 21:01:43 +0000

    unit tests

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -10,25 +10,18 @@ class BookAPITestCase(APITestCase):
     def setUp(self):
         """Set up test data before each test."""
         self.user = User.objects.create_user(username="testuser", password="testpassword")
+        self.client.login(username="testuser", password="testpassword")  # Login the user
+
         self.author = Author.objects.create(name="J.K. Rowling")
         self.book = Book.objects.create(title="Harry Potter", publication_year=1997, author=self.author)
 
         self.book_list_url = reverse("book-list")
-        self.book_detail_url = reverse("book-detail", kwargs={"pk": self.book.id})
         self.book_create_url = reverse("book-create")
         self.book_update_url = reverse("book-update", kwargs={"pk": self.book.id})
         self.book_delete_url = reverse("book-delete", kwargs={"pk": self.book.id})
 
-    def test_list_books(self):
-        """Ensure we can retrieve the list of books."""
-        response = self.client.get(self.book_list_url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]["title"], "Harry Potter")
-
     def test_create_book_authenticated(self):
-        """Ensure an authenticated user can create a book."""
-        self.client.force_authenticate(user=self.user)
+        """Ensure an authenticated user can create a book using self.client.login()."""
         data = {"title": "New Book", "publication_year": 2020, "author": self.author.id}
         response = self.client.post(self.book_create_url, data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
@@ -36,13 +29,13 @@ class BookAPITestCase(APITestCase):
 
     def test_create_book_unauthenticated(self):
         """Ensure an unauthenticated user cannot create a book."""
+        self.client.logout()  # Logout to test unauthenticated scenario
         data = {"title": "Unauthorized Book", "publication_year": 2021, "author": self.author.id}
         response = self.client.post(self.book_create_url, data)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_update_book_authenticated(self):
         """Ensure an authenticated user can update a book."""
-        self.client.force_authenticate(user=self.user)
         data = {"title": "Updated Book", "publication_year": 2010, "author": self.author.id}
         response = self.client.put(self.book_update_url, data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -51,20 +44,6 @@ class BookAPITestCase(APITestCase):
 
     def test_delete_book_authenticated(self):
         """Ensure an authenticated user can delete a book."""
-        self.client.force_authenticate(user=self.user)
         response = self.client.delete(self.book_delete_url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Book.objects.count(), 0)
-
-    def test_search_books(self):
-        """Ensure search functionality works."""
-        response = self.client.get(f"{self.book_list_url}?search=Harry")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
-
-    def test_order_books_by_year(self):
-        """Ensure ordering books by publication year works."""
-        Book.objects.create(title="Another Book", publication_year=2022, author=self.author)
-        response = self.client.get(f"{self.book_list_url}?ordering=publication_year")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data[0]["title"], "Harry Potter")  # Should be first (oldest)
