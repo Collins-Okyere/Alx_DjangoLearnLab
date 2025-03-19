6aabe6f7a01904fb8f3494b3d26d855d4bb94589
CwequKeys <cagyeiofficial@gmail.com>
2025-03-19 13:25:44 +0000

    tagging functionality added

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ( register_view, login_view, logout_view, profile_view, profile_update_view, tagged_posts, PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, CommentCreateView, CommentUpdateView, CommentDeleteView, PostByTagListView )
+from .views import ( register_view, login_view, logout_view, profile_view, profile_update_view, tagged_posts, PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, CommentCreateView, CommentUpdateView, CommentDeleteView, PostByTagListView, SearchResultsView )
 
 urlpatterns = [
 
@@ -18,6 +18,7 @@ urlpatterns = [
     path("post/<int:pk>/comments/new/", CommentCreateView.as_view(), name="add_comment"),
     path("comment/<int:pk>/update/", CommentUpdateView.as_view(), name="comment_update"),
     path("comment/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment_delete"),
+    path('search/', SearchResultsView.as_view(), name='search_results'),  # ✅ Add this
 
     path("", PostListView.as_view(), name="post_list"),
     path('tags/<slug:tag_slug>/', PostByTagListView.as_view(), name='post_by_tag'),
