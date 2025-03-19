2b8a1ac81819c9f4a9f77fc9e98ae73facf2aa52
CwequKeys <cagyeiofficial@gmail.com>
2025-03-19 12:41:09 +0000

    blog posts views added

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import register_view, login_view, logout_view, profile_view, profile_update_view
+from .views import (PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView)
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -8,4 +9,11 @@ urlpatterns = [
     path("logout/", logout_view, name="logout"),
     path("profile/", profile_view, name="profile"),
     path("profile/update/", profile_update_view, name="profile_update"),
+
+    path("", PostListView.as_view(), name="post_list"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
+    path("post/new/", PostCreateView.as_view(), name="post_create"),
+    path("post/<int:pk>/edit/", PostUpdateView.as_view(), name="post_update"),
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post_delete"),
+    
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -4,6 +4,51 @@ from django.contrib.auth.forms import AuthenticationForm
 from .forms import RegisterForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
+    ordering = ["-published_date"]
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ["title", "content"]
+    template_name = "blog/post_form.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ["title", "content"]
+    template_name = "blog/post_form.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = "blog/post_confirm_delete.html"
+    success_url = "/"
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
 
 
 
