d1dc4db89fa08e2427e50188f986c1e9e31e8e3b
CwequKeys <cagyeiofficial@gmail.com>
2025-03-19 13:17:14 +0000

    tagging functionality added

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Comment, Post
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from taggit.forms import TagWidget
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -32,6 +33,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(attrs={'placeholder': 'Add tags separated by commas'}),
+        }
 
     def save(self, commit=True):
         post = super().save(commit=False)
