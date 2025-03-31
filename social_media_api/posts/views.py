7a59df493c37e8f6c69c9a953a0490b8b3acbe07
CwequKeys <cagyeiofficial@gmail.com>
2025-03-31 22:47:49 +0000

    mistakes corrected

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -7,8 +7,9 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework import generics, status
 from .models import Post, Like
 from rest_framework.permissions import IsAuthenticated
-from django.shortcuts import get_object_or_404
 from notifications.models import Notification
+from django.shortcuts import get_object_or_404
+
 
 
 class PostPagination(PageNumberPagination):
@@ -46,14 +47,15 @@ def user_feed(request):
     serializer = PostSerializer(posts, many=True)
     return Response(serializer.data)
 
+
 class LikePostView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, pk):
         post = get_object_or_404(Post, pk=pk)
-        
+
         like, created = Like.objects.get_or_create(user=request.user, post=post)
-        
+
         if created:
             Notification.objects.create(
                 recipient=post.author, 
@@ -62,7 +64,7 @@ class LikePostView(generics.GenericAPIView):
                 target=post
             )
             return Response({"message": "Post liked."}, status=status.HTTP_201_CREATED)
-        
+
         return Response({"message": "You have already liked this post."}, status=status.HTTP_400_BAD_REQUEST)
 
 class UnlikePostView(generics.GenericAPIView):
@@ -70,12 +72,11 @@ class UnlikePostView(generics.GenericAPIView):
 
     def post(self, request, pk):
         post = get_object_or_404(Post, pk=pk)
-        
-        # Try to  the like object
+
         like = Like.objects.filter(user=request.user, post=post).first()
-        
+
         if like:
             like.delete()
             return Response({"message": "Post unliked."}, status=status.HTTP_200_OK)
-        
+
         return Response({"message": "You haven't liked this post yet."}, status=status.HTTP_400_BAD_REQUEST)
