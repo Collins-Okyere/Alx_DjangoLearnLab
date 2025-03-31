707b69cbce7223179dd090d682791f0cb6185199
CwequKeys <cagyeiofficial@gmail.com>
2025-03-31 23:57:51 +0000

    deployed

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,20 +1,29 @@
-# posts/models.py
-
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils import timezone
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 User = get_user_model()
 
 class Post(models.Model):
-    # Assuming you already have a Post model
     title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(default=timezone.now)
-
+    
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post.title}"
+
 
 class Like(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
