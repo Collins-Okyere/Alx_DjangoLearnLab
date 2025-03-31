59ad67666af75ef9266e279604af9871f03d6a6e
CwequKeys <cagyeiofficial@gmail.com>
2025-03-31 22:44:15 +0000

    mistakes corrected

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -8,6 +8,9 @@ from rest_framework.views import APIView
 from rest_framework import status
 from .models import Post, Like
 from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+from notifications.models import Notification
+
 
 class PostPagination(PageNumberPagination):
     page_size = 10
